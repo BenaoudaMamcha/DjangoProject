04e7fa6a2979c3bcbebfc209a58d1855bb8d8881
BenaoudaMamcha <b.mamchaoui13@gmail.com>
2023-03-08 15:46:42 +0100

    new test with cars

--- my_site/cars/models.py
+++ my_site/cars/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class Cars(models.Model):
+    name=models.CharField(_("matricule"), max_length=50)
