35fe0b182155321f45d19eb3f6017aa83eb40163
BenaoudaMamcha <b.mamchaoui13@gmail.com>
2023-03-08 15:57:18 +0100

    third commit

--- my_site/cars/models.py
+++ my_site/cars/models.py
@@ -4,3 +4,4 @@ from django.utils.translation import gettext as _
 # Create your models here.
 class Cars(models.Model):
     name=models.CharField(_("matricule"), max_length=50)
+    brand = models.CharField(_("marque"), max_length=50, null=True)
